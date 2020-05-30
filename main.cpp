aa877fac893d7882f4a1b45d4796b1a91b2603ea
KostasTas <kostas2113@hotmail.com>
2020-05-30 19:03:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,10 +18,10 @@ int main(){
 	while(1){
             system("cls");
             char ans;
-            cout<<"   |     AutoBooklet   |"<<endl;
-            cout<<"   *-------------------*\n\n"<<endl;
-            cout<<"[1] : pros8iki oximatos       [2] : service"<<endl;
-            cout<<"[3] : Func 3                  [4] : Func 4"<<endl;
+            cout<<"   |   AutoBooklet   |"<<endl;
+            cout<<"   *-----------------*\n\n"<<endl;
+            cout<<"[1] : O logariasmos mou       [2] : Service"<<endl;
+            cout<<"[3] : Fuel                    [4] : Func 4"<<endl;
             cout<<"[5] : Exit\n"<<endl;
             cout<<"->";
             cin>>ans;
